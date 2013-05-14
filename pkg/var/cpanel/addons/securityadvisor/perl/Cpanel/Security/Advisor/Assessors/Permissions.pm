cf94ce7213523e6ba6fee72f90fa1fbc69f0780c
SSP Dev <example@example.com>
2013-05-14 13:56:20 -0500

    both /etc/shadow and /etc/passwd are checked now

--- pkg/var/cpanel/addons/securityadvisor/perl/Cpanel/Security/Advisor/Assessors/Permissions.pm
+++ pkg/var/cpanel/addons/securityadvisor/perl/Cpanel/Security/Advisor/Assessors/Permissions.pm
@@ -20,29 +20,35 @@ sub _check_for_unsafe_permissions {
 
     my $security_advisor_obj = $self->{'security_advisor_obj'};
 
-    # Warn if /etc/shadow is world readable, world writeable, or world executable
-    my $mode = (stat('/etc/shadow'))[2];
-    if ($mode & 007) {
-        $security_advisor_obj->add_advise(
-            {
-                'type' => $Cpanel::Security::Advisor::ADVISE_BAD,
-                'text' => ['/etc/shadow has unsafe permissions'],
-                'suggestion' => ['Reset the permissions on /etc/shadow']
-            }
-        );
-    }
+    my @files = qw( /etc/shadow /etc/passwd );
 
-    # Warn if /etc/shadow has a user or group which is not root
-    my ($uid, $gid) = (stat('/etc/shadow'))[4,5];
+    # Warn if /etc/shadow is world readable, world writable, or world executable,
+    # or if /etc/passwd is world writable
+    for my $file (@files) {
+        my $mode = (stat($file))[2];
 
-    if ($uid != 0 or $gid != 0) {
-        $security_advisor_obj->add_advise(
-            {
-                'type' => $Cpanel::Security::Advisor::ADVISE_BAD,
-                'text' => ['/etc/shadow is owned by a user and/or group which is not root'],
-                'suggestion' => ['Reset the ownership permissions on /etc/shadow']
-            }
-        );
+        if (($file eq '/etc/shadow' and $mode & 007) or ($file eq '/etc/passwd' and $mode & 002)) {
+            $security_advisor_obj->add_advise(
+                {
+                    'type' => $Cpanel::Security::Advisor::ADVISE_BAD,
+                    'text' => ["$file has unsafe permissions"],
+                    'suggestion' => ["Reset the permissions on $file"]
+                }
+            );
+        }
+
+        # Warn if /etc/shadow or /etc/passwd aren't root.root
+        my ($uid, $gid) = (stat($file))[4,5];
+
+        if ($uid != 0 or $gid != 0) {
+            $security_advisor_obj->add_advise(
+                {
+                    'type' => $Cpanel::Security::Advisor::ADVISE_BAD,
+                    'text' => ["$file is owned by a user and/or group which is not root"],
+                    'suggestion' => ["Reset the ownership permissions on $file"]
+                }
+            );
+        }
     }
 }
 
